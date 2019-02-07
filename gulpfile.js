c8acf677f7d510466a367f4ffa670a82bc476826
Thor Christopher Arisland <tcarisland@Thors-MBP.lan>
2019-02-07 21:35:13 +0100

    flatten dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 gulp.task('copy-html', function() {
     return gulp.src('src/index.html')
@@ -8,8 +9,9 @@ gulp.task('copy-html', function() {
 
 gulp.task('minify-js', function() {
     return gulp.src(['src/js/*.js'])
+	.pipe(concat('all.js'))
 	.pipe(uglify())
-	.pipe(gulp.dest('dist/js'));
+	.pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', gulp.series(gulp.parallel('copy-html', 'minify-js')));
