324fd752510c7350f954162d446ae6f3f89812a0
Thor Christopher Arisland <tcarisland@Thors-MBP.lan>
2019-02-07 21:22:21 +0100

    js added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
+gulp.task('copy-html', function() {
     return gulp.src('src/index.html')
-	.pipe(gulp.dest('dist/'));
+	.pipe(gulp.dest('dist'));
 });
+
+gulp.task('minify-js', function() {
+    return gulp.src(['src/js/*.js'])
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', gulp.series(gulp.parallel('copy-html', 'minify-js')));
